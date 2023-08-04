6086f975b53cb11f7ae1a6b660b6d3ceb72391ff
TommyYu <yuchingpanatwork@gmail.com>
2023-08-04 15:35:27 +0800

    Added json export functionality

--- python/NATO_CODE/main.py
+++ python/NATO_CODE/main.py
@@ -1,12 +1,39 @@
 #TODO 1. Create a dictionary in this format:
 
 import pandas as pd
+import time
 
 file_path = "./nato_phonetic_alphabet.csv"
 data = pd.read_csv(file_path)
 itr = data.iterrows()
-my_dict = {row_data.letter:row_data.code for index,row_data in itr}
+# iterrows returns data in the form of <index>.<column>
+my_dict = {row_data.letter : row_data.code for index, row_data in itr}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-# prompt = input("What's the word you have?")
+# while True:
+#     try:
+#         prompt = input("What's the word you have?").upper()
+#         code = [my_dict[letter] for letter in prompt]
+#     except KeyError as key:
+#         print(f"Sorry. {key} is not a valid input.")
+#     else:
+#         print(code)
+
+def generate_phonetic():
+    inp = "123"  # Fixed value to mock input
+    try:
+        output_list = [my_dict[letter] for letter in inp]
+    except KeyError:
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+before = time.time()
+try:
+    generate_phonetic()
+except RecursionError:
+    after = time.time()
+    print(f"This took just {round(after - before, 3)} seconds to crash!")
+
--- python/password-manager-start/main.py
+++ python/password-manager-start/main.py
@@ -3,6 +3,8 @@ from tkinter import messagebox
 from PIL import ImageTk
 import pyperclip
 import random as r
+import json
+
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -16,24 +18,53 @@ def password_generator(letter=8, number=5, symbols=3):
     password = "".join(password)
     password_textbox.insert(0, password)
     pyperclip.copy(password)
-    messagebox.showinfo(title="Success!", message=f"{password} is now in your clipboard and ready for use.")
+    messagebox.showinfo(title="Success!", message=f"{password} is now on your clipboard and ready for use.")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def update_json(new_dict):
+    with open(file="./password_content.json", mode="r") as new_file:
+        data = json.load(new_file)
+        data.update(new_dict)
+
+    with open (file="./password_content.json", mode="w") as new_file:
+        json.dump(data,new_file, indent=4)
+
+def search_json():
+    string = website_text.get()
+    try:
+        with open('./password_content.json', 'r') as file:
+            data = json.load(file)
+        messagebox.showinfo(title="Record Found", message=f"Please check below:\n {data[string]}")
+    except KeyError as key:
+        messagebox.showerror(title="Error", message=f"{key} not found in the database")
+
+
+
 def save_password():
     website = website_text.get()
     username = user_name_text.get()
     password = password_text.get()
-
+    json_dict = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showwarning(title="Warning", message="You've left some fields blank")
     else:
-        is_okay = messagebox.askyesno(title="Please confirm the entry", message=f"Website: {website} \n" \
+        is_okay = messagebox.askyesno(title="Please confirm the entry", message=f"Website: {website} \n"
                                                                       f"username: {username} \n"
                                                                       f"password: {password}\n"
                                                                       f"Click Yes to confirm entry")
         if is_okay:
-            with open(file="./password_content.txt", mode="a", encoding="utf-8") as f:
-                f.write(f"Website: {website} | Username: {username} | Password: {password}\n")
+            try:
+                update_json(json_dict)
+            except FileNotFoundError as file_name:
+                with open(file=file_name.filename, mode="w", encoding="utf-8") as f:
+                    json.dump(json_dict, f, indent=4)
+            finally:
                 website_textbox.delete(0, END)
                 user_name_textbox.delete(0, END)
                 password_textbox.delete(0, END)
@@ -42,7 +73,6 @@ def save_password():
 root = Tk()
 root.title("Password manager")
 root.geometry('600x400')
-# root.config(padx= 100, pady=100)
 
 lock_img_frame = ImageTk.PhotoImage(file='logo.png')
 padlock_img = Label(image=lock_img_frame)
@@ -52,26 +82,29 @@ website_label = Label(text='Website:')
 website_label.grid(column=0, row=1)
 
 website_text = StringVar()
-website_textbox = Entry(textvariable=website_text, width=52)
-website_textbox.grid(column=1, row=1, columnspan=2, sticky='we')
+website_textbox = Entry(textvariable=website_text, width=26)
+website_textbox.grid(column=1, row=1, sticky='we')
 website_textbox.focus()
+
+search_button = Button(text='Search', command=search_json)
+search_button.grid(column=2, row=1, sticky='we')
 
 user_name_label = Label(text='Email/Username:')
 user_name_label.grid(column=0, row=2)
 
 user_name_text = StringVar()
-user_name_textbox = Entry(textvariable=user_name_text, width=52)
+user_name_textbox = Entry(textvariable=user_name_text, width=26)
 user_name_textbox.grid(column=1, row=2, columnspan=2, sticky='we')
 
 password_label = Label(text='Password:')
 password_label.grid(column=0, row=3)
 
 password_text = StringVar()
-password_textbox = Entry(textvariable=password_text, width=35)
+password_textbox = Entry(textvariable=password_text, width=17)
 password_textbox.grid(column=1, row=3, sticky='we')
 
 generate_password_button = Button(text="Generate Password", command=password_generator)
-generate_password_button.grid(column=2, row=3)
+generate_password_button.grid(column=2, row=3, sticky='we')
 
 add_button = Button(text="Add", width=44, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
