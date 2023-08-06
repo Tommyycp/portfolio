f90bebcbe273464c0bb8b1551ac209f5d219e868
TommyYu <yuchingpanatwork@gmail.com>
2023-08-07 00:13:39 +0800

    Flash-card-project

---
+++ python/flash-card-project-start/main.py
@@ -0,0 +1,92 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkmacosx import Button as NewButton
+import pandas as pd
+import random as r
+
+BACKGROUND_COLOR = "#B1DDC6"
+BACKGROUND_COLOR_BACK = "#9BC0B0"
+SCORE = 0
+CURRENT_WORD = None
+GUESSED_WORD = []
+
+# Operation
+df = pd.read_csv("./data/french_words.csv")
+data_dict = {r.French: r.English for i, r in df.iterrows()}
+french = list(data_dict.keys())
+
+
+def flip_front():
+    global CURRENT_WORD
+    guess = r.choice(french)
+    if guess in GUESSED_WORD:
+        flip_front()
+    else:
+        card_front_label.config(image=card_front_img)
+        title.config(text="French", bg='white')
+        CURRENT_WORD = guess
+        word.config(text=guess, bg='white')
+
+
+def tick():
+    global SCORE
+    global GUESSED_WORD
+    if CURRENT_WORD in GUESSED_WORD:
+        pass
+    else:
+        GUESSED_WORD.append(CURRENT_WORD)
+        SCORE += 1
+    print(SCORE)
+    print(GUESSED_WORD)
+
+
+def wrong():
+    pass
+
+
+def flip_back():
+    global CURRENT_WORD
+    translation = data_dict[CURRENT_WORD]
+    card_front_label.config(image=card_back_img)
+    title.config(text='English', bg=BACKGROUND_COLOR_BACK)
+    word.config(text=translation, bg=BACKGROUND_COLOR_BACK)
+
+
+def main_game(number=1):
+    if number > 0:
+        flip_back()
+    elif number < 0:
+        flip_front()
+    root.after(5000, main_game, number * -1)
+
+
+# GUI
+
+root = Tk()
+root.config(bg=BACKGROUND_COLOR)
+root.title("French Word Flashcard")
+
+card_front_img = ImageTk.PhotoImage(Image.open("./images/card_front.png"))
+card_back_img = ImageTk.PhotoImage(Image.open("./images/card_back.png"))
+
+card_front_label = Label(root, image=card_front_img, width=800, height=526)
+card_front_label.grid(column=0, row=0, columnspan=2, padx=50, pady=50)
+
+title = Label(text='French', font=('Ariel', 60, 'italic'), bg='white', fg='black', )
+title.place(x=360, y=150)
+
+word = Label(text='Word', font=('Ariel', 80, 'bold'), bg='white', fg='black')
+word.place(x=340, y=300)
+
+wrong_img = ImageTk.PhotoImage(Image.open("./images/wrong.png"))
+button_cross = NewButton(root, image=wrong_img, command=wrong, bg=BACKGROUND_COLOR)
+button_cross.grid(column=0, row=1)
+
+tick_img = ImageTk.PhotoImage(Image.open("./images/right.png"))
+button_tick = NewButton(root, image=tick_img, command=tick, bg=BACKGROUND_COLOR)
+button_tick.grid(column=1, row=1)
+
+root.after(1000, flip_front)
+root.after(3000, main_game)
+
+root.mainloop()
